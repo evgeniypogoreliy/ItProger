26dbd3046b6ff5c445427ab4276ef07bbaa239a0
Evgeny <joy08@mail.ru>
2022-03-02 17:11:01 +1100

    Функции и работа с файломи

---
+++ ITPROGER_Lesson_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program{
+
+    static void Main(){
+
+        short[] numbers = new short[10];
+        short summa = 0;
+        
+        Random random = new Random();
+        for(byte i = 0; i < numbers.Length; i++){
+            
+            numbers[i] = Convert.ToInt16( random.Next(5, 15));
+            System.Console.WriteLine("Elem: " + numbers[i]);
+            summa += numbers[i];
+        }
+        System.Console.WriteLine("Summa: " + summa);
+    }
+}
---
+++ ITPROGER_Lesson_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program{
+
+    public static void Main(){
+        // System.Console.WriteLine("Введите текст");
+        // string text = Console.ReadLine();
+        // using(FileStream stream = new FileStream("info1.txt", FileMode.OpenOrCreate)){
+        //     byte[] array = System.Text.Encoding.Default.GetBytes(text);
+
+        //     stream.Write(array, 0, array.Length);
+        // }
+
+        using(FileStream stream1 = File.OpenRead("info1.txt" )) {
+            byte[] array = new byte[stream1.Length];
+
+            stream1.Read(array, 0, array.Length);
+
+            string textFromFile = System.Text.Encoding.Default.GetString(array);
+            System.Console.WriteLine(textFromFile); 
+        }
+    }
+}
