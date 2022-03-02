e9f2b8c4828ff069223bda8a6a18b3da10eea737
Evgeny <joy08@mail.ru>
2022-03-02 15:52:48 +1100

    Функции

---
+++ ITPROGER_Lesson_6/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        byte[] nums = {5, 7, 8, 3, 5};
+        byte res1 = Summa(nums);
+        System.Console.WriteLine("Результат: "+ res1);
+
+        byte[] nums1 = {5, 9, 8, 3, 5};
+        byte res2 = Summa(nums1);
+        System.Console.WriteLine("Результат: "+ res2);
+    } 
+    
+    public static byte Summa(byte[] digits)
+    {
+        byte summ = 0;
+        foreach(byte el in digits)
+        summ += el;
+
+        return summ; 
+    }
+       
+}
