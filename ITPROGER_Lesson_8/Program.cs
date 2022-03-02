0eb69fb8f17f7a73e039f0e139ef8a79b7a65754
Evgeny <joy08@mail.ru>
2022-03-03 10:08:40 +1100

    Обработчик исключений

---
+++ ITPROGER_Lesson_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program{
+
+    static void Main(){
+
+        try {
+            int a, b, res;
+            Console.Write("Введите первое число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            res = a / b;
+            System.Console.WriteLine("Результат: " + res);
+        }catch(DivideByZeroException){
+            Console.WriteLine("Деление на ноль!");
+        }catch(FormatException){
+            Console.WriteLine("Вы ввели не тот формат!");
+        }finally{
+            Console.WriteLine("Конец программы!");
+        }
+    }
+}
